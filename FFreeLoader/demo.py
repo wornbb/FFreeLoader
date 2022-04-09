5cbf4692b5a63542150c620ea1d01665ac165509
wornbb <yishen.dev@gmail.com>
2022-04-09 01:38:41 -0400

    demo 2

--- FFreeLoader/demo.py
+++ FFreeLoader/demo.py
@@ -1,8 +1,11 @@
-from lib2to3.pygram import Symbols
-from symtable import Symbol
 import yahoo_client
+import json
+
 configuration = yahoo_client.Configuration()
-configuration.api_key['X-API-KEY'] = 'YOUR_API_KEY'
+configuration.api_key['X-API-KEY'] = 'anEGPki60w9uSjIkjNNtl2ZGFGsNF5GKakfMA7L0'
 api_instance = yahoo_client.APIApi(yahoo_client.ApiClient(configuration))
-symbols='AAPL'
+symbols='AAPL,MSFT' # no space between symbols
 api_instance.quotes(symbols)
+data = json.loads(api_instance.api_client.last_response.data.decode())
+
+a = "stop"
